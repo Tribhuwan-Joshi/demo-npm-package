2e075bdb6fedfd3d83bdf00166d55f9387889703
Tribhuwan <mathincosmos@gmail.com>
2022-09-07 20:47:25 +0530

    add demo package for npm

---
+++ README.md
@@ -0,0 +1,2 @@
+##Hamburger bar
+- A ss
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css'
+const ham = document.querySelector('.ham');
+ham.addEventListener("click", showMenu)
+
+function showMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("visible")
+}
